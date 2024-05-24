e204adf12aadb5c5e70676efea634569bae55707
Field Peerapong <me@peerapong.dev>
2024-05-24 19:05:58 +0700

    turbo

--- cra/src/bootstrap.js
+++ cra/src/bootstrap.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// This weird hack is needed because sometime the remote is initialized before
+// the shared dependency
 import("./App").then(({ default: App }) => {
   const root = ReactDOM.createRoot(document.getElementById("root"));
   root.render(
